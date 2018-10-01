f5da61f75293ad9bcb344ef6bac40ad0d1d772ec
Nil Shah <nilshah.31@gmail.com>
2018-10-01 22:02:51 +0530

    Changes: Added Config File, Added Nodemon module for track dev server changes

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
+const config     = require('./config.json')
 const express    = require('express')
-const app        = express()
-const port       = 8001
 const routes     = require('./routes/routes.js')
 const bodyParser = require('body-parser');
 const mongoose   = require('mongoose');
+const app        = express()
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+/**
+ * MONGO configuration
+ */
 //Set up default mongoose connection
-var mongoDB = 'mongodb://127.0.0.1/todo';
+var mongoDB = config.db.db_url+"/"+config.db.db_name;
 mongoose.connect(mongoDB,{ useNewUrlParser: true });
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
 //Get the default connection
 var db = mongoose.connection;
-global.mongoConnection = db;
-
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+/**
+ * Routes and server Configuration
+ */
 app.use('/api', routes);
-app.listen(port, () => console.log(`Application Running on the Port : ${port}!`))
+app.listen(config.dev.port, () => console.log(`Application Running on the Port : ${config.dev.port}!`))
 
 
